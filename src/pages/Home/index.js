4479d506fe0eebe4b0dc9d0273442dfb9a759b26
viniciuiges <viniciusiges@gmail.com>
2023-06-19 14:51:15 -0300

    Adicionando projeto ao repositorio, adicionando a react-router-don, react-icons e iniciando as rotas

---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+export default function Home () {
+    return(
+      <div className="app">
+        <h1>Page home</h1>
+      </div>
+    )
+  }
---
+++ src/pages/Links/index.js
@@ -0,0 +1,7 @@
+export default function Links () {
+    return(
+      <div className="app">
+        <h1>Page Link</h1>
+      </div>
+    )
+  }
