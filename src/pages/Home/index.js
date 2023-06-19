e5cfdebff42405102fc91a2d30208a836dd6008f
viniciuiges <viniciusiges@gmail.com>
2023-06-19 16:46:18 -0300

    Estilizando a page Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,22 @@
+import { FiLink } from 'react-icons/fi';
+
+import './home.css';
+
 export default function Home () {
     return(
-      <div className="app">
-        <h1>Page home</h1>
+      <div className="container-home">
+        <div className="logo">
+            <img src="/logo.png" alt="Logo meu Link" />
+            <h1>Meu Link</h1>
+            <span>Cole seu link para encurtar </span>
+        </div>
+        <div className="area-input">
+            <div>
+                <FiLink size={24} color='#fff' />
+                <input placeholder='Cole seu link aqui...' />
+            </div>
+            <button>Gerar Link</button>
+        </div>
       </div>
     )
   }
